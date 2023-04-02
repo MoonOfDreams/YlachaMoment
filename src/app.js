8a1aaeb6cf206f52eacef500c894c8b5c7981266
Rosbel <rosbelg15@gmail.com>
2023-04-02 13:15:08 -0300

    archivos iniciales

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+const path = require("path")
+app.use(express.static("public")
+)
+const router=require("./routers/mainRouters.js");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+app.use(router);
+
+app.listen(3000, ()=>{
+    console.log("listo para ejecutar "+"http://localhost:3000/")
+})
---
+++ src/controllers/mainControllers.js
@@ -0,0 +1,7 @@
+const fs=require("fs");
+const path=require("path");
+module.exports={
+    index:(Req,res)=>{
+    res.render("home")
+    }
+}
