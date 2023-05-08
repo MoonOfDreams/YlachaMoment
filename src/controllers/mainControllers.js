560e4f598833f9f157d300ee30c8e1f159b047c1
MoonOfDreams <rosbelg15@gmail.com>
2023-05-08 18:57:53 -0300

    agrego archivos

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -3,5 +3,17 @@ const path=require("path");
 module.exports={
     index:(Req,res)=>{
     res.render("home")
+    },
+    login:(Req,res)=>{
+    res.render("login")
+    },
+    register:(Req,res)=>{
+    res.render("register")
+    },
+    adopta:(Req,res)=>{
+    res.render("adopta")
+    },
+    nuestraYlacha:(Req,res)=>{
+    res.render("nuestraYlacha")
     }
 }
--- src/routers/mainRouters.js
+++ src/routers/mainRouters.js
@@ -4,6 +4,10 @@ const controllers=require("../controllers/mainControllers");
 
   
 router.get("/", controllers.index);
+router.get("/register", controllers.register);
+router.get("/login", controllers.login);
+router.get("/adopta", controllers.adopta);
+router.get("/nuestraYlacha", controllers.nuestraYlacha);
 
 
 module.exports = router;
