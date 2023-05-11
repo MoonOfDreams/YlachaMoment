18009f1b96456b58b4daa1cf011d19e4fb05c3db
Hexanima <nicolasfilippelli22@gmail.com>
2023-05-10 21:20:19 -0300

    Archivos iniciales

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-const express=require("express");
-const app=express();
-const path = require("path")
-app.use(express.static("public")
-)
-const router=require("./routers/mainRouters.js");
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static("public"));
+const router = require("./routers/mainRouters.js");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use(router);
 
-app.listen(3000, ()=>{
-    console.log("listo para ejecutar "+"http://localhost:3000/")
-})
+app.listen(3000, () => {
+  console.log("listo para ejecutar " + "http://localhost:3000/");
+});
